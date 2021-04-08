728d99a630d334b1b28d81cc92e69107d094838c
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-04-08 11:18:06 +0900

    Create Manager.java

---
+++ CodeStuff/payroll/Manager.java
@@ -0,0 +1,24 @@
+public class Manager extends SalariedEmployee {
+	
+	private double bonus;
+	
+	public Manager(String name, String ssn,
+			double weeklySalary) {
+		
+		super(name, ssn, weeklySalary);
+		
+		bonus = 0.0;
+	}
+	
+	// getter, setter method 넣기
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	
+	// override method earnings in CommissionEmployee
+	@Override
+	public double getEarnings() {
+		return super.getEarnings() + bonus;
+	}
+	
+}
